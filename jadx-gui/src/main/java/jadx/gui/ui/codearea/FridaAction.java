059458aa26f24a61e70dc724b26704c5b014a312
dorry <jadyzdr@gmail.com>
2022-01-07 22:41:40 +0800

    add script class all methods

--- jadx-gui/src/main/java/jadx/gui/ui/codearea/FridaAction.java
+++ jadx-gui/src/main/java/jadx/gui/ui/codearea/FridaAction.java
@@ -9,11 +9,13 @@ import javax.swing.KeyStroke;
 
 import org.jetbrains.annotations.Nullable;
 
-import jadx.api.JavaMethod;
 import jadx.core.dex.info.MethodInfo;
 import jadx.core.dex.instructions.args.ArgType;
+import jadx.core.dex.nodes.ClassNode;
+import jadx.core.dex.nodes.MethodNode;
 import jadx.gui.treemodel.JNode;
 import jadx.gui.utils.UiUtils;
+import jadx.gui.treemodel.JClass;
 import jadx.gui.treemodel.JMethod;
 import static javax.swing.KeyStroke.getKeyStroke;
 
@@ -33,105 +35,151 @@ public final class FridaAction extends JNodeMenuAction<JNode> {
 		});
 	}
 
-	private static String convertType(String type){
-        if (!type.endsWith("[]")) {
-            return type;
-        }
-        String typeResult = "";
-        while (type.endsWith("[]")) {
-            typeResult += "[";
-            type = type.substring(0, type.length()-2);
-        }
-        switch (type){
-            case "int":
-                typeResult += "I";
-                break;
-            case "boolean":
-                typeResult += "Z";
-                break;
-            case "byte":
-                typeResult += "B";
-                break;
-            case "char":
-                typeResult += "C";
-                break;
-            case "double":
-                typeResult += "D";
-                break;
-            case "float":
-                typeResult += "F";
-                break;
-            case "long":
-                typeResult += "J";
-                break;
-            case "short":
-                typeResult += "S";
-                break;
-            default:
-                typeResult += "L"+type + ";";
-                break;
-        }
-        return typeResult;
-    }
-	
+	private static String convertType(String type) {
+		if (!type.endsWith("[]")) {
+			return type;
+		}
+		String typeResult = "";
+		while (type.endsWith("[]")) {
+			typeResult += "[";
+			type = type.substring(0, type.length() - 2);
+		}
+		switch (type) {
+			case "int":
+				typeResult += "I";
+				break;
+			case "boolean":
+				typeResult += "Z";
+				break;
+			case "byte":
+				typeResult += "B";
+				break;
+			case "char":
+				typeResult += "C";
+				break;
+			case "double":
+				typeResult += "D";
+				break;
+			case "float":
+				typeResult += "F";
+				break;
+			case "long":
+				typeResult += "J";
+				break;
+			case "short":
+				typeResult += "S";
+				break;
+			default:
+				typeResult += "L" + type + ";";
+				break;
+		}
+		return typeResult;
+	}
+
+	private String formatClassName(MethodInfo methodInfo) {
+		//String className = methodInfo.getDeclClass().getType().getObject();
+		String className = methodInfo.getDeclClass().getAliasShortName();
+		if(className.length() <= 3) {
+			className = methodInfo.getDeclClass().getAliasFullName();
+		}
+		String classNameVar = className.replace("$", "_");
+		classNameVar = classNameVar.replace("-", "_");
+		classNameVar = classNameVar.replace(".", "_");
+		classNameVar += "_cls";
+		return classNameVar;
+	}
+
+	private String generateClassDefinition(MethodInfo methodInfo, String classNameVar) {
+		String className = methodInfo.getDeclClass().getType().getObject();
+		StringBuffer sb = new StringBuffer();
+		sb.append("var ");
+		sb.append(classNameVar);
+		sb.append(" = Java.use('");
+		sb.append(className);
+		sb.append("');\n");
+		return sb.toString();
+	}
+
+	private String generateMethod(MethodInfo methodInfo, String classNameVar) {
+		if (methodInfo.isClassInit()) {
+			return "";
+		}
+		String methodName = methodInfo.getName();
+		List<ArgType> argTypes = methodInfo.getArgumentsTypes();
+		String arguments = "";
+		String params = "";
+		for (int i = 0; i < argTypes.size(); i++) {
+			ArgType arg = argTypes.get(i);
+			arguments = arguments + "'" + convertType(arg.toString()) + "', ";
+			params = params + "v" + i + ", ";
+		}
+		if (arguments.length() > 0) {
+			arguments = arguments.substring(0, arguments.length() - 2);
+			params = params.substring(0, params.length() - 2);
+
+		}
+		if (methodInfo.isConstructor()) {
+			methodName = "$init";
+		}
+		StringBuffer sb = new StringBuffer();
+		sb.append(classNameVar);
+		sb.append(".");
+		sb.append(methodName);
+		sb.append(".overload(");
+		sb.append(arguments);
+		sb.append(").implementation = function(");
+		sb.append(params);
+		sb.append(") {\n    let invokeId = INVOKEID++;\n    ");
+		if (methodInfo.isConstructor() == false && methodInfo.getReturnType().isVoid() == false) {
+			sb.append("var ret = ");
+		}
+		sb.append("this.");
+		sb.append(methodName);
+		sb.append("(");
+		sb.append(params);
+		sb.append(");\n    printMethod(invokeId, false, false, arguments, ret, '");
+		sb.append(methodInfo.toString());
+		sb.append("');\n");
+		if (methodInfo.isConstructor() == false && methodInfo.getReturnType().isVoid() == false) {
+			sb.append("    return ret;\n");
+		}
+		sb.append("}\n");
+		return sb.toString();
+	}
+
 	private void doFrida() {
 		if (node != null) {
+			System.out.println(node.getClass().getName());
+			String classNameVar = "";
+			String classDefStr = "";
+			String methodStr = "";
+			boolean canSet = false;
 			if (node.getClass().getName().equals(JMethod.class.getName())) {
+				canSet = true;
 				JMethod jMethod = (JMethod) node;
-				JavaMethod javaMethod = jMethod.getJavaMethod();
-				MethodInfo methodInfo = javaMethod.getMethodNode().getMethodInfo();
-				String className = methodInfo.getDeclClass().getType().getObject();
-				String methodName = methodInfo.getName();
-				List<ArgType> argTypes = methodInfo.getArgumentsTypes();
-				String arguments = "";
-				String params = "";
-				for (int i = 0; i < argTypes.size(); i++) {
-					ArgType arg = argTypes.get(i);
-					arguments = arguments +"'" + convertType(arg.toString()) + "', ";
-					params = params +"v"+i + ", ";
+				MethodInfo methodInfo = jMethod.getJavaMethod().getMethodNode().getMethodInfo();
+				classNameVar = formatClassName(methodInfo);
+				classDefStr = generateClassDefinition(methodInfo, classNameVar);
+				methodStr = generateMethod(methodInfo, classNameVar);
+			} else if (node.getClass().getName().equals(JClass.class.getName())) {
+				canSet = true;
+				JClass jClass = (JClass) node;
+				ClassNode clsNode = jClass.getCls().getClassNode();
+				List<MethodNode> methods = clsNode.getMethods();
+				for (int i = 0; i < methods.size(); i++) {
+					MethodNode method = methods.get(i);
+					MethodInfo methodInfo = method.getMethodInfo();
+					if (i == 0) {
+						classNameVar = formatClassName(methodInfo);
+						classDefStr = generateClassDefinition(methodInfo, classNameVar);
+					}
+					methodStr += generateMethod(methodInfo, classNameVar);
 				}
-				if(arguments.length() > 0) {
-					arguments = arguments.substring(0, arguments.length() - 2);
-					params = params.substring(0, params.length() -2);
-					
-				}
-				if(methodInfo.isConstructor()){
-					methodName = "$init";
-				}
-				StringBuffer sb = new StringBuffer();
-				String classNameVar = className.replace("$","_");
-				classNameVar = classNameVar.replace("-","_");
-				classNameVar = classNameVar.replace(".","_");
-				classNameVar += "_cls";
-				sb.append("var ");
-				sb.append(classNameVar);
-				sb.append(" = Java.use('");
-				sb.append(className);
-				sb.append("');\n");
-				sb.append(classNameVar);
-				sb.append(".");
-				sb.append(methodName);
-				sb.append(".overload(");
-				sb.append(arguments);
-				sb.append(").implementation = function(");
-				sb.append(params);
-				sb.append(") {\n    let invokeId = INVOKEID++;\n    ");
-				if(methodInfo.isConstructor() == false && methodInfo.getReturnType().isVoid() == false) {
-					sb.append("var ret = ");
-				}
-				sb.append("this.");
-				sb.append(methodName);
-				sb.append("(");
-				sb.append(params);
-				sb.append(");\n    printMethod(invokeId, false, false, arguments, ret ,'");
-				sb.append(methodInfo.toString());
-				sb.append("');\n");
-				if(methodInfo.isConstructor() == false && methodInfo.getReturnType().isVoid() == false) {
-					sb.append("    return ret;\n");
-				}
-				sb.append("}");
-				System.out.println(sb.toString());
-				UiUtils.setClipboardString(sb.toString());
+			}
+			if (canSet) {
+				String resultStr = classDefStr + methodStr;
+				System.out.println(resultStr);
+				UiUtils.setClipboardString(resultStr);
 			}
 			node = null;
 		}
